526ccb3674e241ed41df8eadbf64ce6946d93733
Rachit Makwana <rachitmakwana15@gmail.com>
2026-07-27 20:19:35 +0530

    Update migration dependencies for speakersociallink

--- app/eventyay/base/migrations/0048_speakersociallink.py
+++ app/eventyay/base/migrations/0048_speakersociallink.py
@@ -1,5 +1,3 @@
-# Generated manually for SpeakerSocialLink
-
 import django.db.models.deletion
 from django.db import migrations, models
 
@@ -7,7 +5,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0046_user_show_publicly_default_false'),
+        ('base', '0047_team_can_manage_social_media'),
     ]
 
     operations = [
