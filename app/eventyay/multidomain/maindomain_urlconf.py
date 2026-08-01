77f231df5b36517fedb0abdf846c1b845c7b7793
Rachit Makwana <rachitmakwana15@gmail.com>
2026-08-01 18:34:19 +0530

    Remove all-events slug: add direct /upcoming/ and /past/ routes, add redirects for old URLs

--- app/eventyay/multidomain/maindomain_urlconf.py
+++ app/eventyay/multidomain/maindomain_urlconf.py
@@ -3,6 +3,7 @@ import logging
 
 from django.apps import apps
 from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
 
 from eventyay.common.urls import OrganizerSlugConverter  # noqa: F401 (registers converter)
 
@@ -34,10 +35,13 @@ presale_patterns_main = [
             (
                 locale_patterns
                 + [
-                    path('all-events/upcoming/', UpcomingEventsView.as_view(), name='events.upcoming'),
-                    path('all-events/past/', PastEventsView.as_view(), name='events.past'),
+                    path('upcoming/', UpcomingEventsView.as_view(), name='events.upcoming'),
+                    path('past/', PastEventsView.as_view(), name='events.past'),
                     path('followed-events/', FollowedEventsView.as_view(), name='events.followed'),
                     path('<orgslug:organizer>/', include(organizer_patterns)),
+                    path('all-events/upcoming/', RedirectView.as_view(url='/upcoming/', permanent=True)),
+                    path('all-events/past/', RedirectView.as_view(url='/past/', permanent=True)),
+                    path('all-events/', RedirectView.as_view(url='/', permanent=True)),
                     path(
                         '<orgslug:organizer>/<slug:event>/',
                         include(event_patterns),
