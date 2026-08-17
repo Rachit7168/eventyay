43c4bd7ff582f5503abc7c9f80031603084eeafd
Rachit Makwana <rachitmakwana15@gmail.com>
2026-08-17 12:15:51 +0530

    Fix schema generation KeyError for OrderSerializer context

--- app/eventyay/api/serializers/order.py
+++ app/eventyay/api/serializers/order.py
@@ -840,16 +840,17 @@ class OrderSerializer(I18nAwareModelSerializer):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if not self.context['request'].query_params.get('pdf_data', 'false') == 'true':
-            self.fields['positions'].child.fields.pop('pdf_data', None)
+        if 'request' in self.context:
+            if not self.context['request'].query_params.get('pdf_data', 'false') == 'true':
+                self.fields['positions'].child.fields.pop('pdf_data', None)
 
-        for exclude_field in self.context['request'].query_params.getlist('exclude'):
-            p = exclude_field.split('.')
-            if p[0] in self.fields:
-                if len(p) == 1:
-                    del self.fields[p[0]]
-                elif len(p) == 2:
-                    self.fields[p[0]].child.fields.pop(p[1])
+            for exclude_field in self.context['request'].query_params.getlist('exclude'):
+                p = exclude_field.split('.')
+                if p[0] in self.fields:
+                    if len(p) == 1:
+                        del self.fields[p[0]]
+                    elif len(p) == 2:
+                        self.fields[p[0]].child.fields.pop(p[1])
 
     def validate_locale(self, l):
         if l not in set(k for k in self.instance.event.settings.locales):
